8759e88b642b6a7c95d23558762605851674a65c
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-23 12:37:16 -0300

    Jogo
    HTML_CSS_Base_inicial _JS

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe'); 
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() =>{
+
+    const pipePosition = pipe.offsetLeft;
+    if (pipePosition >= 70){
+
+        pipe.style.animation ='none';
+
+    }
+}, 10);
+
+
+
+document.addEventListener('keydown',jump);
